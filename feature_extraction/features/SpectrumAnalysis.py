1db2004c1f78017ac577585d9755382ed986936d
XU-TY <Tianyu.Xu@eleves.ec-nantes.fr>
2021-03-24 03:15:44 +0100

    SpectrumAnalysis

--- feature_extraction/features/SpectrumAnalysis.py
+++ feature_extraction/features/SpectrumAnalysis.py
@@ -1,32 +1,28 @@
 import pylab as pl
 import numpy as np
 import csv
+
+
 from IIRFilter import LowPassIIR
 
 # This is to determine the frequency range of the interference
-# Channel 8 is the best to see the plot
 
 def main():
 
-    IIR = LowPassIIR(a=0.7)
-
     with open('emg.csv','r') as f:
         reader = csv.reader(f)
-        data = [row[7] for row in reader]
+        data = [row[4] for row in reader]
+        data = list(map(float,data))
 
     fs = 2000
     t = [x/2000.0 for x in range(2000)]
-
-    # filter
-    for i in range(0,2000):
-        data_fil = list(map(float,data))
-        data_fil[i] = IIR.filter(data_fil[i])
 
     # original signal
     pl.subplot(221)
     pl.plot(t,data)
     pl.xlabel('time(s)')
     pl.title("original signal")
+
     # Sampling points
     N = len(t)
     # Resolution
@@ -37,22 +33,23 @@ def main():
     # *2/N reflects the relationship between the result of
     # FFT transformation and the actual signal amplitude
     absY = [np.abs(x) for x in Y]
-    # the modulus of the Fourier transform result
-    # Spectrum analysis
+    # Plot fft before filter
     pl.subplot(223)
     pl.plot(f,absY)
     pl.xlabel('freq(Hz)')
     pl.title("fft before filter")
+    pl.ylim((0,10))
+
+    # filter
+    IIR = LowPassIIR(a=0.7, y=data)
+    data_fil = IIR.filter(data)
 
     pl.subplot(222)
     pl.plot(t,data_fil)
     pl.xlabel('time(s)')
     pl.title("signal after filter")
-    # Sampling points
     N = len(t)
-    # Resolution
     df = fs/(N-1)
-    # Build frequency array
     f = [df*n for n in range(0,N)]
     Y = np.fft.fft(data_fil)*2/N
     absY = [np.abs(x) for x in Y]
@@ -60,7 +57,7 @@ def main():
     pl.plot(f,absY)
     pl.xlabel('freq(Hz)')
     pl.title("fft after filter")
-
+    pl.ylim((0,10))
     pl.show()
 
 if __name__ == '__main__':
